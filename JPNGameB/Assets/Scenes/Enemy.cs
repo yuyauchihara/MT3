c42409897ecaf832c1c5d1011b68f8bda1b45b39
DESKTOP-F6TLTDN\hk416 <syoei-hanashiro-20f@stu.kbc.ac.jp>
2022-02-08 14:36:20 +0900

    プロトのプロト

---
+++ JPNGameB/Assets/Scenes/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject bulletPre;
+    float count = 0;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        count += Time.deltaTime;
+
+        if (count >= 3.0f)
+        {
+            GameObject bullet = (GameObject)Instantiate(bulletPre, transform.position, Quaternion.identity);
+            Rigidbody2D Bprb = bullet.GetComponent<Rigidbody2D>();
+            Vector2 force = new Vector2(-5500.0f, 0);
+            Bprb.AddForce(force);
+            Destroy(bullet, 5f);
+            count = 0;
+        }
+    }
+}
---
+++ JPNGameB/Assets/Scenes/Move.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    Rigidbody2D Refrb;
+    float moveSpeed = 10.5f;
+    public Material blue;
+    public Material green;
+    bool Guard = false;
+
+    Vector2 Ref = new Vector2(5500, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, 0);
+
+        Vector2 force = new Vector2(0, 6500.0f);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(force);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            GetComponent<Renderer>().material.color = blue.color;
+            Guard = true;
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = green.color;
+            Guard = false;
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (Guard == true)
+        {
+            if (other.gameObject.tag == "bullet")
+            {
+                Refrb = other.gameObject.GetComponent<Rigidbody2D>();
+                Refrb.AddForce(Ref);
+            }
+        }
+
+        if (other.gameObject.tag == "bullet")
+        {
+            Destroy(other.gameObject);
+        }
+
+        if (Guard == false)
+        {
+            if (other.gameObject.tag == "bullet")
+            {
+                Destroy(gameObject);
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
