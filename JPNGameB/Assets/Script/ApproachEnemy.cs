e736f284a015988d68fcec7e8ad5f22ad3df94fd
DESKTOP-F6TLTDN\hk416 <syoei-hanashiro-20f@stu.kbc.ac.jp>
2022-02-17 09:39:18 +0900

    ノックバック未完成

--- JPNGameB/Assets/Script/ApproachEnemy.cs
+++ JPNGameB/Assets/Script/ApproachEnemy.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class ApproachEnemy : MonoBehaviour
 {
+    public GameObject player;
+    Move move;
+    bool moveKnock;
     // Start is called before the first frame update
     void Start()
     {
-        
+        move = player.GetComponent<Move>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(-0.05f, 0, 0);
+        moveKnock = move.KnockFlag;
+        if(moveKnock == false)
+        {
+            transform.Translate(-0.05f, 0, 0);
+        }
+        else
+        {
+            if(moveKnock == true)
+            {
+                transform.Translate(0.1f, 0, 0);
+                moveKnock = false;
+            }
+        }
     }
 }
