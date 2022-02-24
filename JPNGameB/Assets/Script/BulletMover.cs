7f8b79e86f936b9aa19897410f6e65fe15c39194
KBC12005008\kbc12005008 <ryouichi-nakama-20f@stu.kbc.ac.jp>
2022-02-24 14:53:33 +0900

    空飛ぶエネミー追加

---
+++ JPNGameB/Assets/Script/BulletMover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMover : MonoBehaviour
+{
+    //[SerializeField] private Transform playerTrans; //追いかける対象のTransform
+    [SerializeField] private float bulletSpeed;  　 //弾の速度
+    [SerializeField] private float limitSpeed;      //弾の制限速度
+    private Rigidbody2D rb;                         //弾のRigidbody2D
+    private Transform bulletTrans;                  //弾のTransform
+    private GameObject player;
+    Transform gomi;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+        gomi = player.GetComponent<Transform>();
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        bulletTrans = GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 vector3 = gomi.position - bulletTrans.position;  //弾から追いかける対象への方向を計算
+        rb.AddForce(vector3.normalized * bulletSpeed);                  //方向の長さを1に正規化、任意の力をAddForceで加える
+
+        float speedXTemp = Mathf.Clamp(rb.velocity.x, -limitSpeed, limitSpeed);　//X方向の速度を制限
+        float speedYTemp = Mathf.Clamp(rb.velocity.y, -limitSpeed, limitSpeed);  //Y方向の速度を制限
+        rb.velocity = new Vector3(speedXTemp, speedYTemp);　　　　　　　　　　　//実際に制限した値を代入
+    }
+}
