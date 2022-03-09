89b716bf1f44b9ba44869f116d0c232990e6e404
KBC12005021\kbc12005021 <taichi-tomoyose-20f@stu.kbc.ac.jp>
2022-03-09 12:01:03 +0900

    ダメージスクリプト

---
+++ JPNGameB/Assets/Script/PlayerDamage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+public class PlayerDamage : MonoBehaviour
+{
+    public GameObject hpbar;
+    public bool on_damage = false;       //ダメージフラグ
+    private SpriteRenderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "bullet")
+        {
+            
+        }
+    }
+}
