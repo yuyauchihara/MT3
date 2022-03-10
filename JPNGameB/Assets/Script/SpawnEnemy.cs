312bd7ce9e39b26e27f1450e6d7fa31c91ff3d78
KBC12005009\kbc12005009 <kodai-matayoshi-20f@stu.kbc.ac.jp>
2022-03-10 14:14:35 +0900

    敵がスポーンする処理

---
+++ JPNGameB/Assets/Script/SpawnEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public GameObject Spawn;
+
+    void Start()
+    {
+        Spawn.gameObject.SetActive(false);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Spawn.gameObject.SetActive(true);
+        }
+    }
+}
