0a46437d357f988f79df6ca771c6ab2d62176a69
KBC12005008\kbc12005008 <ryouichi-nakama-20f@stu.kbc.ac.jp>
2022-03-29 10:52:45 +0900

    猫

--- JPNGameB/Assets/Script/Enemy613.cs
+++ JPNGameB/Assets/Script/Enemy613.cs
@@ -6,25 +6,50 @@ public class Enemy613 : MonoBehaviour
 {
     public GameObject bulletPre;
     float count = 0;
+    public bool changetype = true;
+    GameObject Player;
+    GameObject Enemy;
+    [SerializeField] private float speed; // 移動スピード]
+    public Vector3 PlayerPos { get; private set; }
+    public Vector3 EnemyPos { get; private set; }
+    public Vector3 move;
 
     void Start()
     {
-
+        this.Player = GameObject.Find("Player");
+        this.Enemy = GameObject.Find("Muzzle1");
+        PlayerPos = this.Player.transform.position;
+        EnemyPos = this.Enemy.transform.position;
+        Aim(Enemy.transform.position, Player.transform.position, speed);
     }
 
     void Update()
     {
-
+        
         count += Time.deltaTime;
 
         if (count >= 3.0f)
         {
             GameObject bullet = (GameObject)Instantiate(bulletPre, transform.position, Quaternion.identity);
             Rigidbody2D Bprb = bullet.GetComponent<Rigidbody2D>();
-            //Vector2 force = new Vector2(-300.0f, -300.0f);
-            //Bprb.AddForce(force);
+
+            // if(changetype == true)
+            // {
+            // Vector2 force = new Vector2(-300.0f, 0);
+            // Bprb.AddForce(force);
+            // }
+            //else if(changetype == false)
+            // {
+            bullet.transform.position += move * Time.deltaTime;
+            //}
+
             Destroy(bullet, 5f);
             count = 0;
         }
+    }
+    void Aim(Vector3 EnemyPos, Vector3 PlayerPos, float speed)
+    {
+        move = (PlayerPos - EnemyPos).normalized * speed;
+
     }
 }
