e7501923c665b5964a34af8f686e1d3887a0a062
KBC12005016\kbc12005016 <yuya-uchihara-20f@stu.kbc.ac.jp>
2022-03-31 01:52:20 +0900

    盾のガイド追加

---
+++ JPNGameB/Assets/Script/Guide.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guide : MonoBehaviour
+{
+    public SpriteRenderer sp;
+    // ダメージ判定フラグ
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        var h = Input.GetAxis("Horizontal");//左スティックの横
+
+        if (Input.GetKey("joystick button 5"))
+        {
+            float level = Mathf.Abs(Mathf.Sin(Time.time * 2));
+            sp.color = new Color(1f, 1f, 1f, level);
+        }
+        else
+        {
+            sp.color = new Color(1f, 1f, 1f, 1f);
+        }
+
+        if (h < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            //transform.localPosition = new Vector2(-1.8f, 0);
+
+        }
+        else if (0 < h)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            //transform.localPosition = new Vector2(1.8f, 0);
+        }
+    }
+
+}
