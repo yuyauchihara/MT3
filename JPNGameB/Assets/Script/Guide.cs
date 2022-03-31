1cded0fb5f61c7f8c94a5963c13429d4d12f4b82
KBC12005016\kbc12005016 <yuya-uchihara-20f@stu.kbc.ac.jp>
2022-03-31 12:36:33 +0900

    プレイヤーが範囲に入ったら敵が弾を撃ってくるようにした

--- JPNGameB/Assets/Script/Guide.cs
+++ JPNGameB/Assets/Script/Guide.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Guide : MonoBehaviour
 {
-    public SpriteRenderer sp;
+    //public SpriteRenderer sp;
     // ダメージ判定フラグ
 
 
@@ -16,27 +16,28 @@ public class Guide : MonoBehaviour
     void Update()
     {
         var h = Input.GetAxis("Horizontal");//左スティックの横
+        var v = Input.GetAxis("JoyVertical");//右スティックの縦 
+        //if (Input.GetKey("joystick button 5"))
+        //{
+        //    float level = Mathf.Abs(Mathf.Sin(Time.time * 2));
+        //    sp.color = new Color(1f, 1f, 1f, level);
+        //}
+        //else
+        //{
+        //    sp.color = new Color(1f, 1f, 1f, 1f);
+        //}
 
-        if (Input.GetKey("joystick button 5"))
-        {
-            float level = Mathf.Abs(Mathf.Sin(Time.time * 2));
-            sp.color = new Color(1f, 1f, 1f, level);
-        }
-        else
-        {
-            sp.color = new Color(1f, 1f, 1f, 1f);
-        }
 
         if (h < 0)
         {
-            transform.rotation = Quaternion.Euler(0, 180, 0);
-            //transform.localPosition = new Vector2(-1.8f, 0);
+            transform.localPosition = new Vector2(-0.8f, 0);
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
 
         }
-        else if (0 < h)
+        if (0 < h)
         {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-            //transform.localPosition = new Vector2(1.8f, 0);
+            transform.localPosition = new Vector2(0.8f, 0);
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
     }
 
--- JPNGameB/Assets/Script/HANA_BulletTogou.cs
+++ JPNGameB/Assets/Script/HANA_BulletTogou.cs
@@ -21,7 +21,12 @@ public class HANA_BulletTogou : MonoBehaviour
 
         count += Time.deltaTime;
 
-        if (count >= 3.0f)
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (count >= 3.0f && collision.gameObject.tag == "Player")
         {
             GameObject bullet = (GameObject)Instantiate(bulletPre, transform.position, Quaternion.identity);
             Rigidbody2D Bprb = bullet.GetComponent<Rigidbody2D>();
