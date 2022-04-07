eb7ce001dfb6978f9f687cd3f39c7950d3fdce59
又吉 <kodai-matayoshi-20f@stu.kbc.ac.jp>
2022-04-07 09:32:06 +0900

    カメラの設定

---
+++ JPNGameB/Assets/Script/Check.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Check : MonoBehaviour
+{
+    public int EnemySousu = 0;
+
+    public static int DeleteEnemy = 0;
+    public static bool BattleEnemyDelete = false;
+
+    void Update()
+    {
+        if(CameraChange.Battle == false)
+        {
+            DeleteEnemy = 0;
+        }
+    }
+    private void CheckStart()
+    {
+        CameraChange.Battle = false;
+        BattleEnemyDelete = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player" && DeleteEnemy == EnemySousu && BattleEnemyDelete)
+        {
+            CheckStart();
+        }
+
+        if (other.tag == "Player" && DeleteEnemy == EnemySousu && BattleEnemyDelete)
+        {
+            CheckStart();
+        }
+    }
+}
