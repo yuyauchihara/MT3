6fdde883ed57a2c0fd413181a7ce57fc6401124f
KBC12005009\kbc12005009 <kodai-matayoshi-20f@stu.kbc.ac.jp>
2022-04-22 12:11:14 +0900

    ステージ選択画面簡易版＆2-1追加

---
+++ JPNGameB/Assets/Script/StageSelect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSelect : MonoBehaviour
+{
+    GameObject Object;
+    SceneChange code;
+
+    void Start()
+    {
+        Object = GameObject.Find("GameObject");
+        code = Object.GetComponent<SceneChange>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey("joystick button 0"))
+        {
+            code.Scene = 0;
+        }
+
+        if(Input.GetKey("joystick button 1"))
+        {
+            code.Scene = 3;
+        }
+
+        if(Input.GetKey("joystick button 3"))
+        {
+            code.Scene = 6;
+        }
+    }
+}
