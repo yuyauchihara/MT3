feb17e8fe974da96aecb8b660d4bf48f6d381942
DESKTOP-F6TLTDN\hk416 <syoei-hanashiro-20f@stu.kbc.ac.jp>
2022-05-04 21:00:57 +0900

    gomi

---
+++ JPNGameB/Assets/Hanashiro_01/FryEnemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FryEnemy : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine("Fry");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(move, 0);
+    }
+
+    IEnumerator Fry()
+    {
+        move = -8f;
+        yield return new WaitForSeconds(3f);
+        move = 0f;
+        yield return new WaitForSeconds(5f);
+    }
+
+}
