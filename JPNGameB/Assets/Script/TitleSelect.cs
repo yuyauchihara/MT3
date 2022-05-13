23fe3ba1257ade5348675c0e9694d0d615eee82a
kbc12005101 <ryuki-china-20f@stu.kbc.ac.jp>
2022-05-13 09:59:38 +0900

    更新

---
+++ JPNGameB/Assets/Script/TitleSelect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleSelect : MonoBehaviour
+{
+
+    public RectTransform a;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        a.position += new Vector3(0.1f, 0, 0);
+    }
+    void OnEnable()
+    {
+        
+
+    }
+}
