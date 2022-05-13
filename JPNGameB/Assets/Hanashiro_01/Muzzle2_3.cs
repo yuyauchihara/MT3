4c2d3f2806df8c8e3fb6ed5069169bd6bddc452e
DESKTOP-F6TLTDN\hk416 <syoei-hanashiro-20f@stu.kbc.ac.jp>
2022-05-13 14:04:24 +0900

    2-3途中

--- JPNGameB/Assets/Hanashiro_01/Muzzle2_3.cs
+++ JPNGameB/Assets/Hanashiro_01/Muzzle2_3.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Muzzle2_3 : MonoBehaviour
 {
     public GameObject bulletPre;
+    public GameObject HeavyBullet;
     public float BulletSped;
     public bool ChangeFlag;
     float count = 0;
@@ -17,6 +18,8 @@ public class Muzzle2_3 : MonoBehaviour
 
     int f = 0;
 
+    private int BurstCount = 0;
+    public static bool isBursted = false;
 
     void Start()
     {
@@ -26,12 +29,18 @@ public class Muzzle2_3 : MonoBehaviour
     private void FixedUpdate()
     {
         count += Time.deltaTime;
-        Debug.Log(ShotCount);
+        Debug.Log(BurstCount);
+
+        if(BurstCount >= 4)
+        {
+            isBursted = true;
+        }
+
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player" && ShotCount < 3 && f == 0)
+        if (collision.gameObject.tag == "Player" && ShotCount < 3 && f == 0 && BurstCount <= 3)
         {
             Burst();
         }
@@ -55,11 +64,12 @@ public class Muzzle2_3 : MonoBehaviour
 
             Destroy(bullet, 5f);
             count = 0;
-        }
+        }      
     }
 
     IEnumerator loopBurst()
     {
+        BurstCount++;
         yield return new WaitForSeconds(3.0f);
         ShotCount = 0;
         f = 0;
