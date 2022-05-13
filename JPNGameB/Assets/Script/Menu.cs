30a17d6548374510a86c9ad01ffb295aa2c2538c
KBC12005009\kbc12005009 <kodai-matayoshi-20f@stu.kbc.ac.jp>
2022-05-13 09:57:47 +0900

    カーソル移動とメニュー画面表示のScript追加

---
+++ JPNGameB/Assets/Script/Menu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private GameObject Panel;
+    private int backP = 0;
+
+    void Start()
+    {
+        Panel.SetActive(false);
+    }
+    void Update()
+    {
+
+        // スタートボタンを押したらポーズメニューを開く、閉じる
+        if (Input.GetKeyDown("joystick button 7") && backP == 0)
+        {
+            Pause();
+            backP++;
+        }
+        else if (Input.GetKeyDown("joystick button 7") && backP == 1)
+        {
+            Resume();
+            backP--;
+        }
+    }
+    private void Pause()
+    {
+        Panel.SetActive(true);
+    }
+
+    private void Resume()
+    {
+        Panel.SetActive(false);
+    }
+}
