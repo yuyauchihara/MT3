d33f8d598c7ecaea0d65680f71f9210968ffc496
kbc12005101 <ryuki-china-20f@stu.kbc.ac.jp>
2022-05-16 14:30:19 +0900

    gomi

--- JPNGameB/Assets/Script/TitleSelect.cs
+++ JPNGameB/Assets/Script/TitleSelect.cs
@@ -7,21 +7,41 @@ public class TitleSelect : MonoBehaviour
 {
 
     public RectTransform a;
+    public GameObject SelectIcon;
+    public int SelectIconPosition;
+
+    bool nowExecCoroutine_ = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SelectIconPosition = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        a.position += new Vector3(0.1f, 0, 0);
+        if (!nowExecCoroutine_)
+        {
+            nowExecCoroutine_ = true;
+            StartCoroutine(SelectIconMove());
+        }
     }
-    void OnEnable()
-    {
-        
 
+    IEnumerator SelectIconMove()
+    {
+
+        if (Input.GetAxisRaw("Vertical2") == 1)
+        {
+            a.position += new Vector3(5f, 0, 0);
+        }
+        else if (Input.GetAxisRaw("Vertical2") == -1)
+        {
+            a.position -= new Vector3(5f, 0, 0);
+        }
+
+        yield return new WaitForSeconds(0.25f);
+
+        nowExecCoroutine_ = false;
     }
 }
---
+++ JPNGameB/Assets/Script/TitleT.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// CanvasGroupコンポーネントがアタッチされていない場合、アタッチ
+[RequireComponent(typeof(CanvasGroup))]
+public class TitleT : MonoBehaviour
+{
+
+    MeshRenderer mesh;
+
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        mesh.material.color = mesh.material.color - new Color32(0, 0, 0, 0);
+        StartCoroutine("Transparent");
+    }
+
+    IEnumerator Transparent()
+    {
+        for (int i = 0; i < 255; i++)
+        {
+            mesh.material.color = mesh.material.color - new Color32(0, 0, 0, 1);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
