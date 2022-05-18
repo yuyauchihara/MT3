3258bc8fcc1631f6a518ad36a9d7566ef6dd6531
KBC12005009\kbc12005009 <kodai-matayoshi-20f@stu.kbc.ac.jp>
2022-05-18 11:41:12 +0900

    カーソルの追加とカーソル移動のScript変更

--- JPNGameB/Assets/Script/Cursor.cs
+++ JPNGameB/Assets/Script/Cursor.cs
@@ -3,36 +3,163 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Cursor : MonoBehaviour
 {
-    int Pos = 1;
+    public int Pos = 1;
     public int nummenu;
     public float linewidth;
 
+    public bool MenuMode = false;
+    public bool TitleMode = false;
+    public bool StageSelectMode = false;
+    public bool GameOverMode = false;
+    public bool ClearMode = false;
+    public bool RetryMode = false;
+
+    private int ModeNum = 0;
+
     void Start()
     {
-
+        if (MenuMode)
+        {
+            ModeNum = 1;
+        }else if (TitleMode)
+        {
+            ModeNum = 2;
+        }
+        else if (StageSelectMode)
+        {
+            ModeNum = 3;
+        }
+        else if (GameOverMode)
+        {
+            ModeNum = 4;
+        }
+        else if (ClearMode)
+        {
+            ModeNum = 5;
+        }
+        else if (RetryMode)
+        {
+            ModeNum = 6;
+        }
     }
 
     void Update()
     {
-        if (Input.GetKeyDown("down") && Pos != nummenu)
+        var bathikaru = Input.GetAxis("Vertical");
+
+        if (Input.GetAxisRaw("Vertical") == 1 && Pos != nummenu)
         {
             Vector3 tmp = this.transform.position;
             this.transform.position = new Vector3(tmp.x, tmp.y - linewidth, tmp.z);
             Pos += 1;
         }
-        else if (Input.GetKeyDown("up") && Pos != 1)
+        else if (Input.GetAxisRaw("Vertical") == -1 && Pos != 1)
         {
             Vector3 tmp = this.transform.position;
             this.transform.position = new Vector3(tmp.x, tmp.y + linewidth, tmp.z);
             Pos -= 1;
         }
-        else if (Input.GetKeyDown("space") && Pos != 1)
+        else if (Input.GetKeyDown("joystick button 0") && Pos != 1)
         {
-            function();
+            switch (ModeNum) {
+                default:
+                    function();
+                    break;
+                case 1:
+                    MenuFunction();
+                    break;
+                case 2:
+                    TitleFunction();
+                    break;
+                case 3:
+                    StageSelectFunction();
+                    break;
+                case 4:
+                    GameOverFunction();
+                    break;
+                case 5:
+                    ClearFunction();
+                    break;
+                case 6:
+                    RetryFunction();
+                    break;
+                
+            }
         }
+
     }
 
     void function()
+    {
+
+    }
+
+    void MenuFunction()
+    {
+        if (Pos == 1)
+        {
+            // 一番上の項目を選んだときの動作
+            // 好きな動作を入れてね
+        }
+        else if (Pos == 2)
+        {
+
+        }
+    }
+
+    void TitleFunction()
+    {
+        if (Pos == 1)
+        {
+            // 一番上の項目を選んだときの動作
+            // 好きな動作を入れてね
+        }
+        else if (Pos == 2)
+        {
+
+        }
+    }
+
+    void StageSelectFunction()
+    {
+        if (Pos == 1)
+        {
+            // 一番上の項目を選んだときの動作
+            // 好きな動作を入れてね
+        }
+        else if (Pos == 2)
+        {
+
+        }
+    }
+
+    void GameOverFunction()
+    {
+        if (Pos == 1)
+        {
+            // 一番上の項目を選んだときの動作
+            // 好きな動作を入れてね
+        }
+        else if (Pos == 2)
+        {
+
+        }
+    }
+
+    void ClearFunction()
+    {
+        if (Pos == 1)
+        {
+            // 一番上の項目を選んだときの動作
+            // 好きな動作を入れてね
+        }
+        else if (Pos == 2)
+        {
+
+        }
+    }
+
+    void RetryFunction()
     {
         if (Pos == 1)
         {
